ca273144ea9b4b4c6fd3bb8df16e997aabbf4145
Constant <110848773+joaopconstant@users.noreply.github.com>
2023-06-16 12:39:54 -0300

    Update LeitoraDados.java

--- src/util/LeitoraDados.java
+++ src/util/LeitoraDados.java
@@ -1,5 +1,31 @@
 package util;
 
+import java.util.Scanner;
+
 public class LeitoraDados {
-    
+    private Scanner scanner;
+
+    public LeitoraDados() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String lerTexto() {
+        return scanner.nextLine();
+    }
+
+    public int lerInt() {
+        return scanner.nextInt();
+    }
+
+    public double lerDouble() {
+        return scanner.nextDouble();
+    }
+
+    public char lerChar() {
+        return scanner.next().charAt(0);
+    }
+
+    public void fecharLeitor() {
+        scanner.close();
+    }
 }
