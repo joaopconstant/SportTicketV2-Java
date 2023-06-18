0530757a7fa0ba7b6bd965bf774ab8c380c97be8
Constant <110848773+joaopconstant@users.noreply.github.com>
2023-06-18 17:57:31 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import programa.Gestor;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Gestor.run();
+        Gestor gestor = new Gestor("partidas.txt");
+        String opcao = "0";
+        
+        while(!(opcao.equals(""))){
+        System.out.println(gestor.exibeOpcoes());
+        opcao = gestor.run();
+        }
     }
 }
